0b3aa718d26876d62051807a8243f4e03789716a
mironenka.katsiaryna <mironenka.katsiaryna.i@gmail.com>
2022-02-09 12:44:42 +0300

    fix issues

--- src/main/java/com/epam/training/epharmacy/controller/util/ControllerUtils.java
+++ src/main/java/com/epam/training/epharmacy/controller/util/ControllerUtils.java
@@ -1,11 +1,25 @@
 package com.epam.training.epharmacy.controller.util;
 
 import com.epam.training.epharmacy.entity.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 
 public class ControllerUtils {
+
+    private static final Logger LOG = LogManager.getLogger(ControllerUtils.class);
+
+    private static final int ITERATION_COUNT = 65536;
+    private static final int KEY_LENGTH = 128;
+    private static final String PBKDF2WITHHMACSHA1_ALGORITHM = "PBKDF2WithHmacSHA1";
 
     private ControllerUtils() {
     }
@@ -16,6 +30,26 @@ public class ControllerUtils {
            return (User) session.getAttribute("user");
         }
         return null;
+    }
+
+    public static byte[] generateHash(final String string) {
+        try {
+            final byte[] salt = getSalt();
+            final KeySpec spec = new PBEKeySpec(string.toCharArray(), salt, ITERATION_COUNT, KEY_LENGTH);
+            final SecretKeyFactory factory = SecretKeyFactory.getInstance(PBKDF2WITHHMACSHA1_ALGORITHM);
+            return factory.generateSecret(spec).getEncoded();
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            LOG.error("Error during generating hash");
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static byte[] getSalt() {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        return salt;
     }
 
 
