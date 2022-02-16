e1b571cd807a146ef89ca996945716b60342edce
mironenka.katsiaryna <mironenka.katsiaryna.i@gmail.com>
2022-02-16 23:13:56 +0300

    fix issues

--- src/main/java/com/epam/training/epharmacy/controller/Controller.java
+++ src/main/java/com/epam/training/epharmacy/controller/Controller.java
@@ -2,6 +2,7 @@ package com.epam.training.epharmacy.controller;
 
 import com.epam.training.epharmacy.controller.constant.ControllerConstants;
 import com.epam.training.epharmacy.controller.exception.PermissionsDeniedException;
+import com.epam.training.epharmacy.dao.connection.ConnectionPool;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -25,6 +26,11 @@ public class Controller extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         process(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        ConnectionPool.getInstance().dispose();
     }
 
     private void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
